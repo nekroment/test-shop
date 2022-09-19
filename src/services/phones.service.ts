e30d9088d93f1ded12459093cc97d38a5a21d4dd
nekroment <adamcevs@gmail.com>
2022-09-19 09:00:14 +0300

    fixed problem with phone rating data

--- src/services/phones.service.ts
+++ src/services/phones.service.ts
@@ -134,7 +134,9 @@ export class PhonesService {
 
   async findPhoneById(id: number): Promise<PhoneModel> {
     const format = this.getRepositoryPhoneFormat();
-    return await format.where('id = :id', { id }).getRawOne();
+    const result = await format.where('id = :id', { id }).getRawOne();
+    result.rating = !result.rating ? 0 : result.rating;
+    return result;
   }
 
   getRepositoryPhoneFormat(): SelectQueryBuilder<Phones> {
